86645389d5449fd1defccfa845339e77d50d9add
Ahmed Adel <ahmedadel0239@gmail.com>
2026-04-24 08:14:10 +0300

    refactor(skillService): update collection name to v2_skillCategories

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -2,7 +2,8 @@ import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import type { SkillCategory } from "@/lib/skills";
 
-const COLLECTION_NAME = "skillCategories";
+// const COLLECTION_NAME = "skillCategories";
+const COLLECTION_NAME = "v2_skillCategories";
 
 // Get all skill categories
 export const getAllSkillCategories = async (): Promise<SkillCategory[]> => {
